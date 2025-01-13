3319f8f0f884dbf366faa65b3610cfa350f5dfea
daniel21703 <daniel217032001@gmail.com>
2025-01-14 00:58:51 +0300

    Update WordPress installation

---
+++ docker/wordpress/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+ls -a
+
+if ! wp core is-installed; then
+    echo "WordPress core is not installed. Installing now..."
+    wp core install --url="$TEST_SITE_URL" --title="$TEST_SITE_TITLE" --admin_user="$TEST_SITE_ADMIN_USER" --admin_email="$TEST_SITE_ADMIN_EMAIL"
+else
+    echo "WordPress core is already installed."
+fi
+
+exec "$@"
