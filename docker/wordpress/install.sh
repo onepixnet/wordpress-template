66006f7c886c646fe993a06febac8c6217965fb6
daniel21703 <daniel217032001@gmail.com>
2025-01-14 21:51:16 +0300

    Clean install.sh

--- docker/wordpress/install.sh
+++ docker/wordpress/install.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-ls -a
-
 if ! wp core is-installed; then
     echo "WordPress core is not installed. Installing now..."
     wp core install --url="$TEST_SITE_URL" --title="$TEST_SITE_TITLE" --admin_user="$TEST_SITE_ADMIN_USER" --admin_email="$TEST_SITE_ADMIN_EMAIL"
