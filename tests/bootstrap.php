4de7a843001618141129b8748d6127bcb6be823b
daniel21703 <daniel217032001@gmail.com>
2025-01-15 22:53:15 +0300

    Apply rector code fixes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,13 +6,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $_tests_dir = getenv( 'WORDPRESS_TEST_DIR' );
 
-if ( ! file_exists( "$_tests_dir/includes/functions.php" ) ) {
-	echo "Could not find $_tests_dir/includes/functions.php, have you run WordPress tests library ?" . PHP_EOL;
+if ( ! file_exists( "{$_tests_dir}/includes/functions.php" ) ) {
+	echo "Could not find {$_tests_dir}/includes/functions.php, have you run WordPress tests library ?" . PHP_EOL;
 	exit( 1 );
 }
 
 // Give access to tests_add_filter() function.
-require_once "$_tests_dir/includes/functions.php";
+require_once "{$_tests_dir}/includes/functions.php";
 
 // Start up the WP testing environment.
-require_once "$_tests_dir/includes/bootstrap.php";
+require_once "{$_tests_dir}/includes/bootstrap.php";
