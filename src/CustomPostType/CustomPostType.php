0be505845ebb07bc3c580a3d42901fc51393afaf
daniel21703 <daniel217032001@gmail.com>
2025-01-17 23:33:29 +0300

    Change CustomPostType classes dependencies

--- src/CustomPostType/CustomPostType.php
+++ src/CustomPostType/CustomPostType.php
@@ -5,19 +5,11 @@ declare( strict_types=1 );
 namespace OnePix\WordPress\CustomPostType;
 
 final class CustomPostType implements PostType {
-	private PostTypeSettings $post_type_settings;
-
 	public function __construct(
 		private string $post_type,
-		?PostTypeSettings $settings = null,
-		private readonly PostTypeRegistrar $registrar = new WordPressPostTypeRegistrar(),
-	) {
-		$this->post_type_settings = $settings ?? new DefaultPostTypeSettings( $this );
-	}
-
-	public function get_post_type(): string {
-		return $this->post_type;
-	}
+		private PostTypeSettings $post_type_settings,
+		private readonly PostTypeRegistrar $registrar
+	) {}
 
 	public function register(): void {
 		$this->registrar->register(
--- src/CustomPostType/PostType.php
+++ src/CustomPostType/PostType.php
@@ -5,7 +5,5 @@ declare( strict_types=1 );
 namespace OnePix\WordPress\CustomPostType;
 
 interface PostType {
-	public function get_post_type(): string;
-
 	public function register(): void;
 }
